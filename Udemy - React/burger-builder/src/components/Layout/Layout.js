c59544d2926eb383142ecc1b133e459b89ee616a
kaczorowski-jakub <54107058+kaczorowski-jakub@users.noreply.github.com>
2021-10-21 10:40:37 +0200

    react fix

--- Udemy - React/burger-builder/src/components/Layout/Layout.js
+++ Udemy - React/burger-builder/src/components/Layout/Layout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/auxi';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
